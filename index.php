5e433f5f15376ce0823cb0e59437347971da5f97
KW1C-KaremakerTimon <124869104+TimonKar@users.noreply.github.com>
2023-03-19 17:40:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,41 @@
+<!--
+    Auteur: Alle Kuperus, Mick Kuijpers en Timon Karemaker
+    Aanmaakdatum: 19/03/2023
 
+    Omschrijving: Guitar Center Zelf Test
+-->
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <title>
+            Guitar Center
+        </title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Koppeling CSS -->
+        <link rel="stylesheet" href="styles/stylesheet.css">
+        <!-- Koppeling JavaScript -->
+        <script src="https://kit.fontawesome.com/ffa74e0cae.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <nav>
+            <!-- <i class="fa-sharp fa-solid fa-bars"></i> -->
+            <section class="logo">
+                <a href="index.html">
+                    <img src="images/logo.png" alt="logo_guitar_center">
+                </a>
+            </section>
+            <section class="searchbar">
+                <form action="#" method="get">
+                    <input type="text" name="search" placeholder="Find Your Sound">
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                </form>
+            </section>
+            <ul>
+                <li>
+
+                </li>
+            </ul>
+        </nav>
+    </body>
+</html>
