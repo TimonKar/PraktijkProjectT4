ab9119437f0ee26e660e74aac6dc47fe712a0fad
KW1C-KaremakerTimon <124869104+TimonKar@users.noreply.github.com>
2023-03-22 13:49:42 +0100

    Add files via upload

---
+++ pages/guitartest.php
@@ -0,0 +1,66 @@
+<!--
+    Auteur: Alle Kuperus, Mick Kuijpers en Timon Karemaker
+    Aanmaakdatum: 19/03/2023
+
+    Omschrijving: Guitar Center Zelf Test
+-->
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <title>
+            Guitar Center
+        </title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Koppeling CSS -->
+        <link rel="stylesheet" href="../styles/navbar.css">
+        <link rel="stylesheet" href="../styles/guitartest.css">
+        <!-- Koppeling JavaScript -->
+        <script src="https://kit.fontawesome.com/ffa74e0cae.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <nav>
+            <!-- <i class="fa-sharp fa-solid fa-bars"></i> -->
+            <section class="logo">
+                <a href="index.html">
+                    <img src="../images/logo.png" alt="logo_guitar_center">
+                </a>
+            </section>
+            <section class="searchbar">
+                <form action="#" method="get">
+                    <input type="text" name="search" placeholder="Find Your Sound">
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                </form>
+            </section>
+            <ul>
+                <li>
+                    <a href="#">Instruments</a>
+                </li>
+                <li>
+                    <a href="pages/guitartest.php">Guitar Test</a>
+                </li>
+                <li>
+                    <a href="#">About Us</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+            <a href="#"><i class="fa-solid fa-user"></i></a>
+        </nav>
+
+        <main>
+            <div class="guitartest-main">
+                <h2>The Guitar Test</h2>
+
+                <p>
+                    Norem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas.
+                </p>
+
+                <button>
+                    Start Now
+                </button>
+            </div>
+        </main>
+    </body>
+</html>
