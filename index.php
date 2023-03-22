61f40e7499d31c3898dd694e32c60c261b4212a6
KW1C-KaremakerTimon <124869104+TimonKar@users.noreply.github.com>
2023-03-22 12:13:55 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -33,9 +33,81 @@
             </section>
             <ul>
                 <li>
-
+                    <a href="#">Instruments</a>
+                </li>
+                <li>
+                    <a href="pages/guitartest.php">Guitar Test</a>
+                </li>
+                <li>
+                    <a href="#">About Us</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
                 </li>
             </ul>
+            <section class="icon">
+                <a href="#"><i class="fa-solid fa-user"></i></a>
+            </section>
         </nav>
+        <main>
+            <section class="container">
+                <section class="box">
+                    <section class="images">
+                        <a href="#"><img src="images/arrival1.png" alt="Mixing"></a>
+                        <a href="#"><img src="images/arrival2.png" alt="Amp"></a>
+                        <a href="#"><img src="images/arrival3.png" alt="Keyboard"></a>
+                        <a href="#"><img src="images/arrival4.png" alt="Mixing"></a>
+                    </section>
+                    <section class="textbox">
+                    <h2>
+                        New Arrivals
+                    </h2>
+                    <p>
+                        Discover the latest and greatest gear
+                    </p>
+                    <a href="#"><button>Shop Now!</button></a>
+                </section>
+                </section>
+                <section class="box">
+                    <section class="image">
+                        <a href="#"><img src="images/lessons.png" alt="Guitar_Lesson"></a>
+                    </section>
+                    <section class="textbox">
+                        <h2>
+                            Get Playing with Lessons
+                        </h2>
+                        <p>
+                            One-on-one instruction with our experts
+                        </p>
+                        <a href="#">Let’s Go ></a>
+                    </section>
+                </section>
+                <section class="box">
+                    <section class="textbox">
+                        <h2>
+                            Shop By Category
+                        </h2>
+                    </section>
+                    <section class="image">
+                        <figure>
+                        <a href="#"><img src="images/acoustic.png" alt="acoustic_guitar"></a>
+                        <figcaption>Acoustic Guitars</figcaption>
+                        </figure>
+                        <figure>
+                        <a href="#"><img src="images/electric.png" alt="electric_guitar"></a>
+                        <figcaption>Electric Guitars</figcaption>
+                        </figure>
+                        <figure>
+                        <a href="#"><img src="images/drums.png" alt="drums"></a>
+                        <figcaption>Drums</figcaption>
+                        </figure>
+                        <figure>
+                        <a href="#"><img src="images/keys.png" alt="keyboard"></a>
+                        <figcaption>Keys</figcaption>
+                        </figure>
+                    </section>
+                </section>
+            </section>
+        </main>
     </body>
 </html>
