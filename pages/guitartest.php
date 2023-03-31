1d34433a5cb0c12fbe7cf20be6d9441f3e0c7e2f
KW1C-TimonKaremaker <timon.karemaker@edu-kw1c.nl>
2023-03-31 08:47:31 +0200

    -

--- pages/guitartest.php
+++ pages/guitartest.php
@@ -13,41 +13,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Koppeling CSS -->
-        <link rel="stylesheet" href="../styles/navbar.css">
+        <link rel="stylesheet" href="../styles/stylesheet.css">
         <link rel="stylesheet" href="../styles/guitartest.css">
         <!-- Koppeling JavaScript -->
         <script src="https://kit.fontawesome.com/ffa74e0cae.js" crossorigin="anonymous"></script>
     </head>
     <body>
-        <nav>
-            <!-- <i class="fa-sharp fa-solid fa-bars"></i> -->
-            <section class="logo">
-                <a href="index.html">
-                    <img src="../images/logo.png" alt="logo_guitar_center">
-                </a>
-            </section>
-            <section class="searchbar">
-                <form action="#" method="get">
-                    <input type="text" name="search" placeholder="Find Your Sound">
-                    <button type="submit"><i class="fa fa-search"></i></button>
-                </form>
-            </section>
-            <ul>
-                <li>
-                    <a href="#">Instruments</a>
-                </li>
-                <li>
-                    <a href="pages/guitartest.php">Guitar Test</a>
-                </li>
-                <li>
-                    <a href="#">About Us</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
-            </ul>
-            <a href="#"><i class="fa-solid fa-user"></i></a>
-        </nav>
+    <?php
+    include "../includes/nav.php"
+    ?>
 
         <main>
             <div class="guitartest-main">
