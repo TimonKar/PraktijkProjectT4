26c17f7f861f2e1b5df4022dd9103ccb9e2835a1
KW1C-TimonKaremaker <timon.karemaker@edu-kw1c.nl>
2023-04-05 11:59:51 +0200

    -

--- pages/process-form.php
+++ pages/process-form.php
@@ -1,1 +1,24 @@
+<!--
+    Auteur: Alle Kuperus, Mick Kuijpers en Timon Karemaker
+    Aanmaakdatum: 19/03/2023
+
+    Omschrijving: Guitar Center Zelf Test
+-->
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+        Guitar Center
+    </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Koppeling CSS -->
+    <link rel="stylesheet" href="../styles/stylesheet.css">
+    <link rel="stylesheet" href="../styles/guitartest.css">
+    <!-- Koppeling JavaScript -->
+    <script src="https://kit.fontawesome.com/ffa74e0cae.js" crossorigin="anonymous"></script>
+</head>
+<body>
 <?php
+include "../includes/nav.php"
+?>
