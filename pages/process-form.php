6f6a0326855a56d6a0954cb5fd7a950eb741f921
KW1C-TimonKaremaker <timon.karemaker@edu-kw1c.nl>
2023-04-06 08:58:54 +0200

    -

--- pages/process-form.php
+++ pages/process-form.php
@@ -20,33 +20,38 @@
 </head>
 <body>
 <?php
-include "../includes/nav.php"
-?>
-<?php
+include "../includes/nav.php";
 
 
-// Get the answers from the form
-$q1 = $_POST['q1'];
-$q2 = $_POST['q2'];
-$q3 = $_POST['q3'];
-$q4 = $_POST['q4'];
-$q5 = $_POST['q5'];
-$q6 = $_POST['q6'];
-$q7 = $_POST['q7'];
+// Check if the form has been submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-// Calculate the result based on the points for each question
-$result = 0;
-$result += $q1;
-$result += array_sum($q2);
-$result += $q3;
-$result += $q4;
-$result += $q5;
-$result += $q6;
-$result += $q7;
+    // Get the answers from the form
+    $q1 = isset($_POST['q1']) ? intval($_POST['q1']) : 0;
+    $q2 = isset($_POST['q2']) ? array_sum(array_map('intval', $_POST['q2'])) : 0;
+    $q3 = isset($_POST['q3']) ? intval($_POST['q3']) : 0;
+    $q4 = isset($_POST['q4']) ? intval($_POST['q4']) : 0;
+    $q5 = isset($_POST['q5']) ? intval($_POST['q5']) : 0;
+    $q6 = isset($_POST['q6']) ? intval($_POST['q6']) : 0;
+    $q7 = isset($_POST['q7']) ? intval($_POST['q7']) : 0;
 
-// Display the result
-echo "Your result is: " . $result;
+    // Calculate the result based on the points for each question
+    $result = $q1 + $q2 + $q3 + $q4 + $q5 + $q6 + $q7;
+
+    // Assign a word based on the total points
+    if ($result >= 70) {
+        $word = "<h3 Fender </h3>";
+    } else if ($result >= 0) {
+        $word = "<h3> Fazly </h3>";
+    } else if ($result >= 3300) {
+        $word = "<h3> Acoustic </h3>";
+    } else {
+        $word = "<h3> Fender </h3> ";
+    }
+
+    // Display the result
+    echo "<h3> The Perfect guitar for you is</h3> " . $word;
+}
 ?>
 
 </body>
-
